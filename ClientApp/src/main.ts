12ff8a612c586b4c28f77df08c277b3b0652ddf6
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-03-20 23:37:37 -0400

    add more components

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,3 +1,5 @@
 ﻿import "./icon.component";
 import "./button.component";
 import "./card.component";
+import "./progress-bar.component";
+import "./spinner.component";
---
+++ ClientApp/src/spinner.component.ts
@@ -0,0 +1,55 @@
+import { html, TemplateResult, render } from "lit-html";
+import { repeat } from "lit-html/lib/repeat";
+import { unsafeHTML } from "lit-html/lib/unsafe-html";
+
+const styles = unsafeHTML(`<style>${require("./spinner.component.css")}<style>`);
+
+export class SpinnerComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes () {
+        return [];
+    }
+
+    connectedCallback() {     
+
+        if(!this.shadowRoot) this.attachShadow({ mode: 'open' });
+        
+		render(this.template, this.shadowRoot);
+
+        if (!this.hasAttribute('role'))
+            this.setAttribute('role', 'spinner');
+
+        this._bind();
+        this._setEventListeners();
+    }
+
+    get template(): TemplateResult {
+        return html`
+            ${styles}
+        `;
+    }
+
+    private async _bind() {
+
+    }
+
+    private _setEventListeners() {
+
+    }
+
+    disconnectedCallback() {
+
+    }
+
+    attributeChangedCallback (name, oldValue, newValue) {
+        switch (name) {
+            default:
+                break;
+        }
+    }
+}
+
+customElements.define(`mdl-spinner`,SpinnerComponent);
