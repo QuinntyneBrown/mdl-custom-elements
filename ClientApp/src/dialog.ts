39763cdec7f428135bec29ed0cb2dae33b0747a8
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-03-23 22:37:51 -0400

    progress bar and dialog

---
+++ ClientApp/src/dialog.ts
@@ -0,0 +1,1 @@
+﻿//https://caniuse.com/#feat=dialog
--- ClientApp/src/progress-bar.component.ts
+++ ClientApp/src/progress-bar.component.ts
@@ -1,55 +1,9 @@
-import { html, TemplateResult, render } from "lit-html";
-import { repeat } from "lit-html/lib/repeat";
-import { unsafeHTML } from "lit-html/lib/unsafe-html";
-
-const styles = unsafeHTML(`<style>${require("./progress-bar.component.css")}<style>`);
-
-export class ProgressBarComponent extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    static get observedAttributes () {
-        return [];
-    }
-
-    connectedCallback() {     
-
-        if(!this.shadowRoot) this.attachShadow({ mode: 'open' });
-        
-		render(this.template, this.shadowRoot);
-
-        if (!this.hasAttribute('role'))
-            this.setAttribute('role', 'progressbar');
-
-        this._bind();
-        this._setEventListeners();
-    }
-
-    get template(): TemplateResult {
-        return html`
-            ${styles}
-        `;
-    }
-
-    private async _bind() {
-
-    }
-
-    private _setEventListeners() {
-
-    }
-
-    disconnectedCallback() {
-
-    }
-
-    attributeChangedCallback (name, oldValue, newValue) {
-        switch (name) {
-            default:
-                break;
-        }
-    }
+export class ProgressBarComponent extends HTMLElement {    
+    connectedCallback() {             
+        this.classList.add("mdl-progress");
+        this.classList.add("mdl-js-progress");
+        this.classList.add("mdl-progress__indeterminate");        
+    }    
 }
 
 customElements.define(`mdl-progress-bar`,ProgressBarComponent);
